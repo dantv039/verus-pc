2fc8deaf523649b59bca37f4f36211fcdf1f1de1
dantv039 <100129873+dantv039@users.noreply.github.com>
2023-07-05 11:29:27 +0700

    Update run_miner.bat

--- hellminer_win64_avx (1)/run_miner.bat
+++ hellminer_win64_avx (1)/run_miner.bat
@@ -3,4 +3,6 @@ REM For more usage info: hellminer.exe --help
 REM Example SSL URL
 REM .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
 
-.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u ADDRESS.WORKER -p x --cpu 1
+.\hellminer.exe -c stratum+tcp://ap.luckpool.net:3956 -u RB9S9VsDw4Lk71iCsALtc7Q8kKHzq2PMMY -p x --cpu 16
+
+
